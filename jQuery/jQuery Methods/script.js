155701eaea10bd1385c2a025e2a1a3b29b181751
Drew <d_overgaard1@u.pacific.edu>
2019-03-31 11:13:40 -0700

    jQuery Methods

---
+++ jQuery/jQuery Methods/script.js
@@ -0,0 +1,23 @@
+$("img").css("width", "200px");
+
+$("img:first-of-type").attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+//$("input").attr("type", "color");
+
+$("img").last().attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+
+$("img").attr("src", "https://upload.wikimedia.org/wikipedia/commons/8/8e/Newfoundland_Pine_Marten.jpg");
+
+$("select").val();
+
+$("h1").addClass("correct");
+
+$("h1").removeClass("correct");
+
+$("li").addClass("wrong");
+
+$("li").removeClass("wrong");
+
+$("li").addClass("correct");
+
+$("li").first().toggleClass("done");
